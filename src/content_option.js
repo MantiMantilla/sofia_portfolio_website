315b19fcc3962dbd7e75e6a7d76da47988f866a8
Alejo Mantilla <alejomantilla@protonmail.com>
2024-07-03 17:24:02 -0600

    Changed a few colors in light theme.

--- src/content_option.js
+++ src/content_option.js
@@ -1,22 +1,22 @@
-const logotext = "JOHN";
+const logotext = "SOFIA";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Sofia Rodríguez",
+    description: "I’m Sofia Rodríguez, communicator, designer, currently working in Bogotá.",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Sofia Rodríguez",
     animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        first: "I love graphic design",
+        second: "I illustrate",
+        third: "I develop products",
     },
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
-    title: "abit about my self",
+    title: "a bit about my self",
     aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
 };
 const worktimeline = [{
